d4b0f389379459c347b74999697bccd7a26e08ec
e3eax <143964705+e3eax@users.noreply.github.com>
2023-11-01 18:31:07 -0400

    Update mergesort.h

--- src/mergesort.h
+++ src/mergesort.h
@@ -4,6 +4,17 @@
 
 namespace hawk {
 
+/**
+ * @brief Sorts a vector of elements using the merge sort algorithm.
+ *
+ * This function sorts a vector of elements in ascending order using the
+ * merge sort algorithm. The merge sort algorithm is a comparison sort that
+ * uses a divide and conquer strategy to efficiently sort the elements of
+ * the vector.
+ *
+ * @tparam T Type of the elements in the vector.
+ * @param v The vector to be sorted.
+ */
 template <typename T>
 void merge_sort(std::vector<T>& v) {
   if (v.size() <= 1) return;
@@ -13,6 +24,20 @@ void merge_sort(std::vector<T>& v) {
   merge_sort_r(v, tmp, 0, v.size() - 1);
 }
 
+/**
+ * @brief Helper function for the merge sort algorithm.
+ *
+ * This function is a recursive helper function for the merge sort algorithm.
+ * It takes a vector, a temporary vector, a left index, and a right index,
+ * and sorts the elements of the vector between the left and right indices
+ * using the merge sort algorithm.
+ *
+ * @tparam T Type of the elements in the vector.
+ * @param v The vector to be sorted.
+ * @param tmp A temporary vector used for merging.
+ * @param l The left index of the range to be sorted.
+ * @param r The right index of the range to be sorted.
+ */
 template <typename T>
 void merge_sort_r(std::vector<T>& v, std::vector<T>& tmp, int l, int r) {
   if (l >= r) return;
@@ -26,6 +51,23 @@ void merge_sort_r(std::vector<T>& v, std::vector<T>& tmp, int l, int r) {
   merge(v, tmp, l, m, r);
 }
 
+/**
+ * @brief Merge function for the merge sort algorithm.
+ *
+ * This function takes a vector, a temporary vector, a left index, a middle
+ * index, and a right index, and merges the elements of the vector between
+ * the left and right indices. The elements between the left and middle
+ * indices are assumed to be sorted, and the elements between the middle
+ * and right indices are also assumed to be sorted. The function merges
+ * these two sorted ranges into a single sorted range.
+ *
+ * @tparam T Type of the elements in the vector.
+ * @param v The vector to be merged.
+ * @param tmp A temporary vector used for merging.
+ * @param l The left index of the range to be merged.
+ * @param m The middle index of the range to be merged.
+ * @param r The right index of the range to be merged.
+ */
 template <typename T>
 void merge(std::vector<T>& v, std::vector<T>& tmp, int l, int m, int r) {
   int i = l;
